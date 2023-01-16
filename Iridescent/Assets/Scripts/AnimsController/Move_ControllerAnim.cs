d339e842b576ba50735c55649010e3aec151b8f1
AdrianAlbarran <adrian-240102@hotmail.es>
2023-01-16 20:53:32 +0100

    animaciones del player hechas

---
+++ Iridescent/Assets/Scripts/AnimsController/Move_ControllerAnim.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_ControllerAnim : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("HorizontalMovement", true);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("HorizontalMovement", false);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- Iridescent/Assets/Scripts/GameObjects/Player.cs
+++ Iridescent/Assets/Scripts/GameObjects/Player.cs
@@ -5,19 +5,22 @@ public class Player : GObject
 {
     public float attackCooldown;
     public bool onAttack;
+    private Animator animator;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         attackCD = attackCooldown;
         moveComponent = new PlayerMoveComponent(this);
         player = this;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
         Move(this.gameObject, Time.fixedDeltaTime);
-
+        animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
     }
 
     public void Update()
